b537e38c6f2beef965de4bdbf6be9c3f7b07bf39
dcynsd <dcynsd@gmail.com>
2020-11-05 16:19:59 +0800

    tag hide

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tag;
-use App\Services\ArchiveService;
-use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
+use App\Services\ArchiveService;
 
 class TagController extends Controller
 {
@@ -16,7 +16,7 @@ class TagController extends Controller
     public function index(Request $request)
     {
         $total = Tag::query()->sum('post_count');
-        $tags = Tag::all()->map(function (Tag $tag) use ($total) {
+        $tags = Tag::all()->where('post_count', '>', 0)->map(function (Tag $tag) use ($total) {
             $tag->color = Arr::random(self::COLOR_MAP);
             $tag->size = $tag->post_count ? round($tag->post_count / $total * 100, 1) : 18;
             return $tag;
